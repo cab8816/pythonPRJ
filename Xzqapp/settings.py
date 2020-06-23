ad66475d37603cac20dfbd50d8d9b58c646f57c0
cab <cab88@139.com>
2020-06-23 15:01:46 +0800

    1

--- Xzqapp/settings.py
+++ Xzqapp/settings.py
@@ -75,12 +75,27 @@ WSGI_APPLICATION = 'Xzqapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+#
+#
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'NAME': 'xzqapp', # 数据库名
+        'USER': 'root', # 账号
+        'PASSWORD': '2533', # 密码
+        'HOST': '127.0.0.1', # HOST
+        'POST': 3306, # 端口
+
     }
 }
+
 
 
 # Password validation
