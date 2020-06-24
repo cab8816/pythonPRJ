86b25fb4b20afebb34c81cdbda7c1cd118c9cdd7
cab <cab88@139.com>
2020-06-24 10:31:43 +0800

    2020-6-24 10:31 增加了数据模型

--- Myapp/views.py
+++ Myapp/views.py
@@ -8,6 +8,6 @@ from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse(u"欢迎你谢振乾1")
-    context={}
-    context['hello']='hello ddd 2020/06/24 10:05 dddddd777 hello tiskk 谢岸马'
-    return render(request,'test.html',context)
+    context = {}
+    context['hello'] = 'hello ddd 2020/06/24 10:05 dddddd777 hello tiskk 谢岸马'
+    return render(request, 'test.html', context)
--- Xzqapp/settings.py
+++ Xzqapp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp.apps.MyappConfig',
+    'Myapp.apps.MyappConfig',
+
+
 ]
 
 MIDDLEWARE = [
