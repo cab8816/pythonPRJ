5c6684502e1efb3f035a462601b44e668bb7cf03
cab <cab88@139.com>
2020-07-02 16:44:36 +0800

    2020-6-24 10:31 增加了doc写得功能

--- Myapp/models.py
+++ Myapp/models.py
@@ -4,3 +4,20 @@ from django.db import models
 # Create your models here.
 class Students(models.Model):
     name = models.CharField(max_length=20)
+
+class Biao4(models.Model):
+    id = models.AutoField(primary_key=True)
+    psxh = models.CharField(max_length=20)
+    lyxh = models.CharField(max_length=20)
+    lyname = models.CharField(max_length=50)
+    lbxh = models.CharField(max_length=20)
+    lb = models.CharField(max_length=50)
+    dxxh = models.CharField(max_length=20)
+    duixiang = models.CharField(max_length=50)
+    xmxh = models.CharField(max_length=20)
+    xmmc = models.CharField(max_length=50)
+    csmc = models.CharField(max_length=50)
+    yjbz = models.CharField(max_length=100)
+    xzfw = models.CharField(max_length=100)
+    sm = models.CharField(max_length=50)
+
