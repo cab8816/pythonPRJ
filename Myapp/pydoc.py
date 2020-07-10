9e42e32b8e2de40537696114e1b659d6f95222b5
cab <cab88@139.com>
2020-07-10 11:35:38 +0800

    2020-6-24 10:31 增加了word 的字体设置

--- Myapp/pydoc.py
+++ Myapp/pydoc.py
@@ -24,7 +24,7 @@ document.add_picture('monty-truth.png', width=Inches(1.25))
 records = (
     (3, '101', 'Spamaa'),
     (7, '422', 'Eggs'),
-    (4, '631', 'Spam, spam, eggs, and 啊spam')
+    (4, '631', 'Spam, spam, eggs, and spam')
 )
 
 table = document.add_table(rows=1, cols=3)
