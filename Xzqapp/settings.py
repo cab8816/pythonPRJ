a786084aaf414e5233546a12844dd8f7bdd040b9
cab <cab88@139.com>
2020-07-24 12:30:39 +0800

    2020-6-24 10:31 增加了word 的字体设置

--- Xzqapp/settings.py
+++ Xzqapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+#  ftp  账号：cab88  254613
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
