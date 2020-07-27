fa9c802d9373d562a05d29420a616c9dc6d0e56e
Administrator <cab88@139.com>
2020-07-27 22:12:21 +0800

    增加css 控制

--- Xzqapp/settings.py
+++ Xzqapp/settings.py
@@ -133,3 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    ('css', os.path.join(STATIC_ROOT, 'css')),
+]
